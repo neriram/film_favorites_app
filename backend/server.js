6c7732c0061e4b678b2cd813d79013173a14b72d
Neri Ramirez <neriram@Neris-MacBook-Air.local>
2021-01-15 16:56:04 -0800

    initializing project

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+//Imports 
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const passportLocal = require("passport-local").Strategy;
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const app = express();
+const User = require("./user");
+
+//Middleware
+
+//Routes
+
+//Start Server
+app.listen(4000, () => {
+    console.log("Server is on");
+});
